798ea6d0135ae9430120a2b9ff927269b6f56b43
LongLN <longln18.nd@gmail.com>
2025-06-06 21:42:51 +0700

    cmt vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/vantai-web/',
+  //  base: '/vantai-web/',
   plugins: [react(), tailwindcss()],
 })
